c5d8e9e49ea70463f75ee4d65fd653877f17bace
izzMalakh <izzaldeenjamal@gmail.com>
2022-10-25 11:54:39 +0300

    -

---
+++ AlfredBot/AlfredQuotes.java
@@ -0,0 +1,60 @@
+import java.util.Date;
+public class AlfredQuotes {
+    
+    public String basicGreeting() {
+        // You do not need to code here, this is an example method
+        return "Hello, lovely to see you. How are you?";
+    }
+    
+    public String guestGreeting(String name) {
+        return "Hello,"+name+",Lovely to see you.";
+        
+    }
+     // NINJA BONUS
+    public String guestGreeting(String name,String dayPeriod) {
+        
+        return "Good"+" "+dayPeriod+","+name+",Lovely to see you.";
+    }
+    public String dateAnnouncement() {
+        Date date=new Date(); 
+        // YOUR CODE HERE
+        return "It is currently," +date;
+    }
+    
+    public String respondBeforeAlexis(String conversation) {
+                if (conversation.contains("Alexis")){
+            return "Right away, sir. She certainly isn't sophisticated enough for that.";
+
+
+        }
+        else if (conversation.contains("Alfred")){
+            return "At your service. As you wish, naturally.";
+
+
+        }
+        else {
+            return "Right. And with that I shall retire.";
+        }
+    }
+    public String respondStausAlfred(String conversation) {
+        // YOUR CODE HERE
+        if (conversation.contains("Angry")){
+            return "Im angry Alexis";
+
+
+        }
+        else if (conversation.contains("Sad")){
+            return "Im Sad Alexis";
+
+
+        }
+        else {
+            return "Alfred ..........";
+        }
+    }
+	// NINJA BONUS
+	// See the specs to overload the guessGreeting method
+    // SENSEI BONUS
+    // Write your own AlfredQuote method using any of the String methods you have learned!
+}
+
---
+++ AlfredBot/AlfredTest.java
@@ -0,0 +1,42 @@
+public class AlfredTest {
+   
+    public static void main(String[] args) {
+        // Make an instance of AlfredQuotes to access all its methods.
+        AlfredQuotes alfredBot = new AlfredQuotes();
+        
+        // Make some test greetings, providing any necessary data
+        String testGreeting = alfredBot.basicGreeting();
+        String testGuestGreeting = alfredBot.guestGreeting("Beth Kane");
+        String testDateAnnouncement = alfredBot.dateAnnouncement();
+        String testGreetingPeriod = alfredBot.guestGreeting("Beth Kane","evening");
+        String alexisTest = alfredBot.respondBeforeAlexis(
+                            "Alexis! Play some low-fi beats."
+                            );
+        String alfredTest = alfredBot.respondBeforeAlexis(
+            "I can't find my yo-yo. Maybe Alfred will know where it is.");
+        String notRelevantTest = alfredBot.respondBeforeAlexis(
+            "Maybe that's what Batman is about. Not winning. But failing.."
+        );
+        // sensi bouns 
+        String respondMoodAlfredAngry =  alfredBot.respondStausAlfred("alfred is  Angry");
+        String respondMoodAlfredSad =  alfredBot.respondStausAlfred("alfred is  Sad");
+        String respondMoodAlfredSomething =  alfredBot.respondStausAlfred("alfred something");
+
+
+
+        // Print the greetings to test.
+        System.out.println(testGreeting);
+        
+        // Uncomment these one at a time as you implement each method.
+        System.out.println(testGuestGreeting);
+        System.out.println(testDateAnnouncement);
+        System.out.println(alexisTest);
+        System.out.println(alfredTest);
+        System.out.println(notRelevantTest);
+        System.out.println(testGreetingPeriod);
+        System.out.println(respondMoodAlfredAngry);
+        System.out.println(respondMoodAlfredSad);
+        System.out.println(respondMoodAlfredSomething);
+
+    }
+}
